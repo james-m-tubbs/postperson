bded76350447717862d6c8e7a84c5f12f9b81815
James Mak-Tubbs <james.m.tubbs@gmail.com>
2018-02-12 10:25:47 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          Hello World!
+        </p>
+      </div>
+
+    );
+  }
+}
+
+export default App;
